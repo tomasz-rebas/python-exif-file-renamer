07332b364fb72dd0c2e8fc587932ef131101ebba
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-05-26 14:11:44 +0200

    Removing get_labeled_exif function (not needed)

--- exif_reader.py
+++ exif_reader.py
@@ -7,13 +7,6 @@ def get_exif(filename):
     image.verify()
     return image._getexif()
 
-# not need to call this one
-def get_labeled_exif(exif):
-    labeled = {}
-    for (key, val) in exif.items():
-        labeled[TAGS.get(key)] = val
-    return labeled
-
 def get_selected_exif(exif):
     selected_data = {'DateTimeOriginal': '', 'MaxApertureValue': '', 'FocalLength': '', 'SubsecTimeOriginal': '', 'ExposureTime': '', 'ISOSpeedRatings': ''}
     for (key, val) in exif.items():
